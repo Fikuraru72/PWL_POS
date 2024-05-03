dad1d182fa90bde9fde65f5a2a463f699632f5c9
fikuraru72 <abiyyufiqlal@gmail.com>
2024-05-03 21:47:02 +0700

    create RestFul API Logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 // Route untuk level user
 Route::get('/levels', [LevelController::class, 'index']);
